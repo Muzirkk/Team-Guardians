4cd78fdc62b889d405743957a96b5b78fe899931
Zhengyang Li <erikyounglee@gmail.com>
2023-05-21 16:04:36 +1000

    @Zhengyang Li - implement Add patient, next of kin, gp page
    @Zhengyang Li - implement Add patient, next of kin, gp page

---
+++ app/src/main/java/com/example/tg_patient_profile/view/general/AddNewPatientNewActivity.java
@@ -0,0 +1,16 @@
+package com.example.tg_patient_profile.view.general;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tg_patient_profile.R;
+
+public class AddNewPatientNewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_patient_new);
+    }
+}
---
+++ app/src/main/java/com/example/tg_patient_profile/view/general/NextOfKinsAddActivity.java
@@ -0,0 +1,16 @@
+package com.example.tg_patient_profile.view.general;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tg_patient_profile.R;
+
+public class NextOfKinsAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_of_kins_add);
+    }
+}
